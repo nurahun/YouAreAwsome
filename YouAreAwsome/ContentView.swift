8988359a91e33a0f743ed7a712778d090ce54245
nur <altun.darwaza@gmail.com>
2025-02-14 08:17:36 -0500

    adedd if statement

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var message1 = "You Are Awsome"
+    @State private var message2 = "You Are Great"
+    @State private var imageName = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+        
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            Button("Press Me") {
+                if message == message1 {
+                    message = message2
+                    imageName = "hand.thumbsup"
+                } else {
+                        message = message1
+                        imageName = "sun.max.fill"
+                    }
+            }
+            
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
         }
         .padding()
     }
