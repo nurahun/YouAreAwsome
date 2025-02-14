4dc11c35cd45c01abf33ef45a97a88dbd4c3144d
nur <altun.darwaza@gmail.com>
2025-02-13 19:31:08 -0500

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Nur Khadr on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
