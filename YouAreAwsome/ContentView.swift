08d5649a9db52beef9f9b659a3b56b467815910b
nur <altun.darwaza@gmail.com>
2025-02-15 09:22:04 -0500

    change image

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -9,14 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var message1 = "You Are Awsome"
-    @State private var message2 = "You Are Great"
-    @State private var imageName = ""
+    @State private var imageString = ""
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: imageName)
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
@@ -28,13 +26,21 @@ struct ContentView: View {
             Spacer()
             
             Button("Press Me") {
-                if message == message1 {
-                    message = message2
-                    imageName = "hand.thumbsup"
-                } else {
-                        message = message1
-                        imageName = "sun.max.fill"
-                    }
+                let message1 = "You Are Awsome!"
+                let message2 = "You Are Great!"
+                let imageString1 = "sun.max.fill"
+                let imageString2 = "hand.thumbsup"
+//                
+//                
+//                if message == message1 {
+//                    message = message2
+//                    imageString = imageString2
+//                } else {
+//                        message = message1
+//                    imageString = imageString1
+//                    }
+                message = (message == message1 ? message2 : message1)
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
             }
             
             .buttonStyle(.borderedProminent)
